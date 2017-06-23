5cd807bbc90217fb541dfc739f00c1786427bcf9
Curnan Reidy <curnanreidy@gmail.com>
2017-06-23 16:02:01 +0100

    Initial commit

---
+++ src/cUtils/Throw.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cUtils
+{
+    public static class Throw<TException> where TException : Exception
+    {
+        public static void If(bool condition, string message)
+        {
+            if (condition)
+                throw (TException)Activator.CreateInstance(typeof(TException), message);
+        }
+    }
+}
