14ae40f63a1cfc2df56beb46089acfa27f475c54
Curnan Reidy <curnanreidy@gmail.com>
2017-06-26 17:19:41 +0100

    Added ps versioning script

--- src/cUtils/Throw.cs
+++ src/cUtils/Throw.cs
@@ -4,8 +4,17 @@ using System.Text;
 
 namespace cUtils
 {
+    /// <summary>
+    /// Throws <typeparamref name="TException"/> if condition is met.
+    /// </summary>
+    /// <typeparam name="TException"></typeparam>
     public static class Throw<TException> where TException : Exception
     {
+        /// <summary>
+        /// If <paramref name="condition"/> is met, exception is thrown.
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message">Exception message</param>
         public static void If(bool condition, string message)
         {
             if (condition)
