54df321a1cd8a9b477de98044a4ffb5a562fa609
dtlins <curnanreidy@gmail.com>
2017-08-26 13:14:47 +0100

    Added Guard class.

---
+++ src/cUtils/Guard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CUtils
+{
+    /// <summary>
+    /// Argument validation class.
+    /// </summary>
+    public static class Guard
+    {
+        /// <param name="argumentValue"></param>
+        /// <param name="argumentName"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [DebuggerHidden]
+        public static void ThrowIfNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+                throw new ArgumentNullException(argumentName);
+        }
+    }
+}
---
+++ test/CUtils.Test/GuardTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CUtils.Test
+{
+    [TestClass]
+    public class GuardTests
+    {
+        [TestMethod]
+        public void Guard_Null_ThrowsArgumentNullException()
+        {
+
+            object argValue = null;
+            var argName = "Arg1";
+
+            Assert.ThrowsException<ArgumentNullException>(() => Guard.ThrowIfNull(argValue, argName));
+        }
+
+        [TestMethod]
+        public void Guard_NotNull_NoException()
+        {
+            object argValue = new object();
+            var argName = "Arg1";
+
+            AssertEx.DoesNotThrowException<Exception>(() => Guard.ThrowIfNull(argValue, argName));
+        }
+    }
+}
